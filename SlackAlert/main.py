80e1d6fdddb279ba75bfef5cb91c49e92273e766
Stuart Graham <stuart@stuart-graham.com>
2020-07-08 12:15:18 +0100

    Add slack boilerplate

--- SlackAlert/main.py
+++ SlackAlert/main.py
@@ -1,5 +1,28 @@
 import json
 import boto3
+import slack
+import os
+
+SLACK_TOKEN = os.environ.get('SLACK_TOKEN','')
+SLACK_CHANNEL = os.environ.get('SLACK_CHANNEL','')
+
+def build_message_block(comment):
+    message = [
+                {
+                    "type": "section",
+                    "text": {
+                        "type": "mrkdwn",
+                        "text": comment
+                    }
+                    },
+            ]
+    return message
+
+def post_to_slack(*args):
+    build_message_block(comment)
+    client = slack.WebClient(token=SLACK_TOKEN)
+    post_message = build_message_block(url, comment)
+    response = client.chat_postMessage(channel=SLACK_CHANNEL, blocks=post_message)
 
 def lambda_handler(event, context):
     pass

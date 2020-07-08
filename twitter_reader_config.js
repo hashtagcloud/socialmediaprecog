355e818c0870adb016a0bf0dfde902fd244bab16
Matthew Milne <63643935+hashtagcloud@users.noreply.github.com>
2020-07-08 12:55:30 +0100

    Add files via upload

---
+++ twitter_reader_config.js
@@ -0,0 +1,11 @@
+var twitter_config = module.exports = {
+twitter: {
+    consumer_key: 'a',
+    consumer_secret: 'b',
+    access_token: 'c',
+    access_token_secret: 'd'
+  },
+topics: ['some', 'keywords', 'go', 'here', 'please'],
+languages: ['en'],
+kinesis_delivery: 'socialmediaingestbot-IngestionFirehoseStream-FPrWxTkHy0lk'
+}
